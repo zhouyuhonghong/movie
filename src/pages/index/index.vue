bcc6f2345aff06ce4fa00a83ff22ed21c5f0c83e
zhouyuhonghong <1304855468@qq.com>
2019-12-19 22:42:26 +0800

    fifth

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,18 @@
 <template>
   <div >
+    <swiper
+      :indicator-dots="indicatorDots"
+      :autoplay="autoplay"
+      :interval="interval"
+      :duration="duration"
+      style="height:620px"
+    >
+      <block v-for="img in imgUrls" :key="img">
+        <swiper-item > 
+          <image :src="img"  style="width:100%;height:100%;"/>
+        </swiper-item>
+      </block>
+    </swiper>
   </div>
 </template>
 
@@ -7,8 +20,19 @@
 export default {
   data () {
     return{
+       imgUrls: [
+        "http://photocdn.sohu.com/20120705/Img347309849.jpg",
+        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 4000,
+      duration: 1000  
     }
   },
+  
 
   methods: {
   },
@@ -19,4 +43,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
